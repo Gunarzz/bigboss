207b7f4c693c91d4f9194e887f0c8d9aaac4ca81
Gunarzz <gunars2006@gmail.com>
2024-04-02 13:16:22 +0300

    ddd

---
+++ main.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/teksts")
+def te():
+    return render_template("index.html")
+
+@app.route("/saraksts")
+def saraksts():
+   saraksts = ["toms", "stankveics", "top"]
+   return render_template("saraksts.html",vardi = saraksts)
+
+if __name__ == '__main__':
+    app.run(port = 5000)
+    
+    print ("sveiki")
